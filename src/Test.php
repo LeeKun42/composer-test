2c35bf016487e70df44cf7111ab7081de7e6ad01
JunLi <980984232@qq.com>
2022-09-07 10:00:23 +0800

    init

---
+++ src/Test.php
@@ -0,0 +1,11 @@
+<?php
+namespace JunLi;
+
+class Test
+{
+    public static function write()
+    {
+        echo 'this is composer lib write';
+        return 'this is composer lib write';
+    }
+}
